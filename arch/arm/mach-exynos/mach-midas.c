7bb98ab8c29d92f30ec8f193ceba59891433e6df
Andrei F <luxneb@gmail.com>
2013-01-27 22:11:41 +0100

    battery: Add MAX77693/Midas charger control

--- arch/arm/mach-exynos/mach-midas.c
+++ arch/arm/mach-exynos/mach-midas.c
@@ -1540,8 +1540,10 @@ static struct samsung_battery_platform_data samsung_battery_pdata = {
 	.chg_curr_siop_lv3 = 475,
 #endif
 
-	.chg_curr_usb = 950,
+	.chg_curr_usb = 475,
+	.in_curr_usb = 475,
 	.chg_curr_cdp = 1000,
+	.in_curr_cdp = 1000,
 #if defined(CONFIG_MACH_T0_USA_VZW)
 	.chg_curr_wpc = 950,
 #else
