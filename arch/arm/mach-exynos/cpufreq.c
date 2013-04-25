0725492ea8c3ea7a121c914690e406545ecec06d
Matrix <ffolkes@ffolkes.com>
2013-04-25 04:51:38 -0400

    cpufreq: fix sticking OC clocks

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -67,12 +67,7 @@ int exynos_verify_speed(struct cpufreq_policy *policy)
 
 unsigned int exynos_getspeed(unsigned int cpu)
 {
-	// return clk_get_rate(exynos_info->cpu_clk) / 1000;
-	unsigned int ret = clk_get_rate(exynos_info->cpu_clk) / 1000;
-	if(ret >= 1940000) ret = 2000000;
-	if(ret == 1920000) ret = 1900000;
-	if(ret == 1704000) ret = 1700000;
-	return ret;
+	return clk_get_rate(exynos_info->cpu_clk) / 1000;
 }
 
 static unsigned int exynos_get_safe_armvolt(unsigned int old_index, unsigned int new_index)
